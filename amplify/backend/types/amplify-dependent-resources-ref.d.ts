1c01e83b351b1ad1081433b7135df226b7b8f64c
Luka <lukafunda1@gmail.com>
2022-05-05 15:18:12 +0200

    storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "imageStorageAmplifyLuka": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
